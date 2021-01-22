3398db214a199e208a94a11ccfbc4047e493569f
CTFallon <c.fallon@cern.ch>
2021-01-22 12:39:58 -0600

    split Ftest from datacard writing

--- Limits/python/datacardsFtest.py
+++ Limits/python/datacardsFtest.py
@@ -7,6 +7,9 @@ import json
 # -processes list is created here
 # files now saved to base directory and exported to EOS after jobs completion
 
+#datacardsFtest.py - part I of seperating ftests from datacard writing
+# this part will create a ws.root file containing the two histograms and two funcs as well as funcs' parameters
+
 
 from ROOT import RooRealVar, RooDataHist, RooArgList, RooGenericPdf, RooBernstein, RooExtendPdf, RooCmdArg, RooWorkspace, RooFit, RooDataSet, RooArgSet, RooCategory, RooFitResult, RooCurve, RooParametricShapeBinPdf
 import os, sys
@@ -89,6 +92,7 @@ isData = True
 
 def getRate(ch, process, ifile):
        hName = ch + "/"+ process
+       print(hName)
        h = ifile.Get(hName)
        #return h.Integral()
        return h.Integral(1,h.GetXaxis().GetNbins()-1)
@@ -566,7 +570,7 @@ def fisherTest(RSS1, RSS2, o1, o2, N):
 #                                                       #
 #*******************************************************#
 
-def getCard(sig, ch, ifilename, outdir, doModelling, mode = "histo", bias = False, verbose = False, doSys = True):
+def getCard(sig, ch, ifilename, outdir, doModelling, mode = "histo", bias = False, verbose = False):
 
 
        ### Cmpute signal efficiencies 
@@ -1234,7 +1238,15 @@ def getCard(sig, ch, ifilename, outdir, doModelling, mode = "histo", bias = Fals
 
                      modelBkg.SetName(modelName)
                      normData.SetName(modelName+"_norm")
-                     wsfilename = "ws.root"
+
+
+                     #*******************************************************#
+                     #                                                       #
+                     #                  Saving RooWorkspace                  #
+                     #                                                       #
+                     #*******************************************************#
+
+                     wsfilename = "ws_{}.root".format(ch_red)
                      wfile_ = ROOT.TFile(wsfilename)
                      w_ = wfile_.FindObjectAny("BackgroundWS")
                      if not w_.__nonzero__() :  w_ = RooWorkspace("BackgroundWS", "workspace")
@@ -1270,283 +1282,6 @@ def getCard(sig, ch, ifilename, outdir, doModelling, mode = "histo", bias = Fals
                             getattr(w_, "import")(modelAlt, RooFit.Rename(modelAlt.GetName())) # Alt func with optimal num Paras
 
                      wstatus = w_.writeToFile(wsfilename, False)
-
-
-
-
-              
-              #*******************************************************#
-              #                                                       #
-              #                    Datacard Writing                   #
-              #                                                       #
-              #*******************************************************#
-              wfile = ROOT.TFile("ws.root")
-              wBkg =  wfile.Get("BackgroundWS")
-              print "workspace ", wBkg.Print()
-              print "Model name: ", modelName
-              modelBkg = wBkg.pdf(modelName)
-              modelAlt = wBkg.pdf(modelAltName)
-              normAlt = wBkg.pdf(modelAltName+"_norm")
-              normData = wBkg.pdf(modelName+"_norm")
-              parSet = modelBkg.getParameters(obsData)
-              parSet.Print()
-              argList = ROOT.RooArgList(parSet)
-              parNames = [ argList[i].GetName() for i in xrange(0, len(parSet))]                                 
-
-
-             # modelExt = RooExtendPdf(modelBkg.GetName(), modelBkg.GetTitle(), modelBkg, normzBkg)
-
-              for i in xrange(0, len(parSet)):
-                    print  argList[i].GetName(), "   ", argList[i].getVal()
-
-
-              # BIAS STUDY
-              # Generate pseudo data
-
-              #setToys = RooDataSet()
-              #setToys.SetName("data_toys")
-              #setToys.SetTitle("Data (toys)")
-
-              #if not isData:
-              #       print " - Generating", nBkgEvts, "events for toy data"
-              #       if bias: setToys = modelAlt.generateBinned(RooArgSet(mT), nBkgEvts, 0,0)
-                     #fitRes = modelBkg.fitTo(setToys, RooFit.Extended(True), RooFit.Save(1), RooFit.SumW2Error(True), RooFit.Strategy(2), RooFit.Minimizer("Minuit2"), RooFit.PrintLevel(1))
-
-              if bias:
-                     ROOT.gSystem.Load("libHiggsAnalysisCombinedLimit.so")
-                     from ROOT import RooMultiPdf
-                     pdf_index_string = "pdf_index_%s" % (ch)
-                     cat = RooCategory(pdf_index_string, "Index of Pdf which is active")
-                     pdfs = RooArgList()
-                     pdfs.add(modelBkg)
-                     pdfs.add(modelAlt)
-                     roomultipdf = RooMultiPdf("roomultipdf", "All Pdfs", cat, pdfs)
-                     #normulti = RooRealVar("roomultipdf_norm", "Number of background events", nDataEvts, 0., 1.e6)
-                     normulti = RooRealVar("roomultipdf_norm", "Number of background events", 1.0, 0., 1.e6)
-
-              # create workspace
-              w = RooWorkspace("SVJ", "workspace")
-              # Dataset
-              # ATT: include isData
-
-              #getattr(w, "import")(obsData, RooFit.Rename("Bkg"))
-              #if isData: getattr(w, "import")(obsData, RooFit.Rename("data_obs"))
-              getattr(w, "import")(obsData, RooFit.Rename("data_obs"))
-              #else: getattr(w, "import")(setToys, RooFit.Rename("data_obs"))
-              #getattr(w, "import")(obsData, RooFit.Rename("data_obs"))
-              getattr(w, "import")(sigData, RooFit.Rename(sig))
-              
-              if bias:
-                     getattr(w, "import")(cat)#, RooFit.Rename(cat.GetName()))
-                     getattr(w, "import")(normulti)#, RooFit.Rename(normulti.GetName()))
-                     getattr(w, "import")(roomultipdf)#, RooFit.Rename(roomultipdf.GetName()))
-
-              for i in xrange(hist.GetNbinsX()):
-                     for year in ["2016","2017","2018"]:
-                            mcstatSysName = "mcstat_%s_%s_MC%sbin%d"  % (ch, sig, year, i+1)
-                            #print mcstatSysName
-                            #print sig + "_" + mcstatSysName + "Up"
-                            mcstatSigUp = getHist(ch, sig + "_" + mcstatSysName + "Up", ifile)
-
-                            #print "Integral  ", mcstatSigUp.Integral()
-                            mcstatSigDown = getHist(ch, sig + "_" + mcstatSysName + "Down", ifile)
-                            mcstatSigHistUp = RooDataHist(sig + "_" + mcstatSysName + "Up", "Data (MC sig)",  RooArgList(mT), mcstatSigUp, 1.)
-                            mcstatSigHistDown = RooDataHist(sig + "_" + mcstatSysName + "Down", "Data (MC sig)",  RooArgList(mT), mcstatSigDown, 1.)
-                            getattr(w, "import")(mcstatSigHistUp, RooFit.Rename(sig + "_" + mcstatSysName + "Up") )
-                            getattr(w, "import")(mcstatSigHistDown, RooFit.Rename(sig + "_" + mcstatSysName + "Down") )
-
-              for sysName,sysValue  in syst.iteritems():
-                     if not ((doSys == True) or (sysName == "lumi")):
-                            continue # always include lumi, and only do others if doSys is True
-                     if(sysValue[0]=="shape" and "mcstat" not in sysName):              
-                            sysUp =  getHist(ch, sig + "_" + sysName + "Up", ifile)
-                            sysDown =  getHist(ch, sig + "_" + sysName + "Down", ifile)
-                            #print "==> Trigg sys name: ", sig + "_" + sysName + "Down"
-                            sysSigHistUp = RooDataHist(sig + "_" + sysName + "Up", sysName + " uncertainty",  RooArgList(mT), sysUp, 1.)
-                            sysSigHistDown = RooDataHist(sig + "_" + sysName + "Down", sysName + " uncertainty",  RooArgList(mT), sysDown, 1.)
-                            getattr(w, "import")(sysSigHistUp, RooFit.Rename(sig + "_" + sysName + "Up") )
-                            getattr(w, "import")(sysSigHistDown, RooFit.Rename(sig + "_" + sysName + "Down") )
-                            
-
-
-              #else: getattr(w, "import")(setToys, RooFit.Rename("data_obs"))
-              getattr(w, "import")(modelBkg, (modelBkg.GetName()))
-              if bias:
-                     getattr(w, "import")(modelAlt, (modelAltName))
-              #getattr(w, "import")(normzBkg, RooFit.Rename(normzBkg.GetName()))
-              wstatus = w.writeToFile("ws_%s_%s_%s.root" % (sig, ch, mode), True)
-
-              if wstatus == False : print "Workspace", "ws_%s_%s_%s.root" % (sig, ch, mode) , "saved successfully"
-              else: print "Workspace", "ws_%s_%s_%s.root" % (sig, ch, mode) , "not saved successfully"
-              workfile = "ws_%s_%s_%s.root" % ( sig, ch, mode)
-              # ======   END MODEL GENERATION   ======       
-              if(verbose):w.Print()
-
-
-       rates = {}
-       procLine = ""
-       procNumbLine = ""
-       rateLine = ""
-       binString = ""
-
-       if(mode == "template"):       
-
-              processes.append("Bkg")
-              processes[:-1] = []
-              rates["Bkg"] = nDataEvts #nBkgEvts, Kevin asked to change to data yield, limits converge better
-              procLine += ("%-43s") % (modelBkg.GetName())
-              rateLine += ("%-43s") % (rates["Bkg"])
-              binString += (("%-43s") % (ch) ) * (2)
-              procNumbLine = 1 
-              
-       else:
-              i = 1
-              bkgrate = 0
-              for p in processes:
-                     rates[p] = getRate(ch, p, ifile)
-                     bkgrate =  rates[p]
-                     procNumbLine += ("%-43s") % (i)
-                     procLine += ("%-43s") % (p)
-                     rateLine += ("%-43.1f") % (bkgrate)
-                     i+=1
-              binString += (("%-43s") % (ch) ) * (len(processes)+1)
-
-
-       rates["data_obs"] = getRate(ch, "data_obs", ifile)
-       rates[sig] = getRate(ch, sig, ifile)
-       if mode == "template":
-              print "TEST sig: ", rates[sig], nSigEvts, binMax
-              print "TEST bkg/obs: ", rates["data_obs"], nDataEvts, binMax
-
-
-
-
-       card  = "imax 1 number of channels \n"
-       card += "jmax * number of backgrounds \n"
-       card += "kmax * number of nuisance parameters\n"
-       card += "-----------------------------------------------------------------------------------\n"
-
-       if(mode == "template"):
-              #card += "shapes   %s  %s    %s    %s    %s\n" % (sig, ch, ifilename, "$CHANNEL/$PROCESS", "$CHANNEL/$PROCESS_SYSTEMATIC")
-              #card += "shapes   %-15s %-5s %s%s.root  %s\n" % (sig, ch, WORKDIR, ch, "SVJ:$PROCESS")
-              card += "shapes   %s  %s    %s    %s\n" % (modelBkg.GetName(), ch, workfile, "SVJ:$PROCESS")
-              card += "shapes   %s  %s    %s    %s    %s\n" % (sig, ch, workfile, "SVJ:$PROCESS", "SVJ:$PROCESS_$SYSTEMATIC")
-              card += "shapes   %s  %s    %s    %s\n" % ("data_obs", ch, workfile, "SVJ:$PROCESS")
-
-       else:  
-              card += "shapes   *      *   %s    %s    %s\n" % (hist_filename, "$CHANNEL/$PROCESS", "$CHANNEL/$PROCESS_$SYSTEMATIC")
-              card += "shapes   data_obs      *   %s    %s\n" % (hist_filename, "$CHANNEL/$PROCESS")
-       card += "-----------------------------------------------------------------------------------\n"
-       card += "bin               %s\n" % ch
-       print "===> Observed data: ", rates["data_obs"]
-       card += "observation       %.6f\n" % (rates["data_obs"]) 
-       card += "-----------------------------------------------------------------------------------\n"
-       card += "bin                                     %-43s\n" % (binString)
-       card += "process                                 %-43s%-43s\n" % (sig, procLine) #"roomultipdf"
-       card += "process                                 %-43s%-43s\n" % ("0", procNumbLine)
-       card += "rate                                    %-43.6f%-43s\n" % (rates[sig], rateLine) #signalYield[m].getVal(), nevents
-       card += "-----------------------------------------------------------------------------------\n"
-
-       for sysName,sysValue  in syst.iteritems():
-              print(sysName, sysValue, "testSystematicValues")
-              if not ((doSys == True) or (sysName == "lumi")):
-                  continue # always include lumi, and only do others if doSys is True
-              if(sysValue[0]=="lnN"): 
-                     card += "%-20s%-20s" % (sysName, sysValue[0])
-                     if(sysValue[1]=="all"and len(sysValue)>2):
-                            if(mode == "template"): card += "%-20s" % (sysValue[2]) * (2)
-                            else: card += "%-20s" % (sysValue[2]) * (len(processes) + 1)
-                     else:
-                            if (sysValue[1]=="all"):
-                                   sysValue[1] = copy.deepcopy(processes)
-                                   sysValue[1].append(sig)
-                                   hsysName =  "_" + sysName  
-                            hsysNameUp = "_" + sysName + "UP"  
-                            hsysNameDown = "_" + sysName + "DOWN" 
-                            if("sig" in sysValue[1]):
-                                   try:
-                                          if(getRate(ch, sig, ifile) != 0.): sigSys = abs((getRate(ch, sig+hsysNameUp, ifile) - getRate(ch, sig+hsysNameDown, ifile))/ (2* getRate(ch, sig, ifile)))
-                                   except AttributeError:
-                                          sigSys = sysValue[2]
-                                   if(sigSys < 1. and sigSys > 0.): sigSys = sigSys + 1
-                                   card += "%-20s" % (sigSys)
-                            else:  card += "%-20s" % ("-")
-                            for p in processes:
-                                   print(p, processes)
-                                   if (p in sysValue[1]):
-                                          if (getRate(ch, p, ifile) != 0.): bkgSys = abs((getRate(ch, p+hsysNameUp, ifile) - getRate(ch, p+hsysNameDown, ifile))/ (2* getRate(ch, p, ifile)) )
-                                          else: bkgSys = 1
-                                          if(bkgSys<1. and bkgSys >0.): bkgSys = bkgSys + 1
-                                          card += "%-20s" % (bkgSys)
-                                   else:  card += "%-20s" % ("-")
-              elif(sysValue[0]=="lnU"):
-                     card += "%-20s%-20s%-20s%-20s" % (sysName, sysValue[0], "-", sysValue[2])
-              elif(sysValue[0]=="shape"):
-                  if("mcstat" not in sysName):
-                            card += "%-20s     shape     " % (sysName)
-                            if ("sig" in sysValue[1]): card += "%-20s" % ( "1") 
-                            else: card += "%-20s" % ( "-") 
-                            for p in processes:
-                                   if (p in sysValue[1]): card += "%-20s" % ( "1") 
-                                   else: card += "%-20s" % ( "-") 
-                  else:
-                                   # CAMBIARE NOME DELLA SYST      , change name of syst?               
-                            for samp in sysValue[1]:
-                                   sampName = ""
-                                   line = ""
-                                   if (samp == "sig" or samp == "Sig"): 
-               
-                                          line = "%-20s" % ( "1") 
-                                          line += "%-20s" % ("-") * (len(processes)) 
-                                          sampName = sig
-                                   elif(mode != "template"):
-                                          line = "%-20s" % ( "-") 
-                                          lineProc = ["%-20s" % ( "-") for x in xrange (len(processes))]
-                                          if samp in processes: 
-                                                 index = processes.index(samp)  
-                                                 lineProc[index] = "1"
-                                          lineProc = "         ".join(lineProc)
-                                          line += lineProc
-                                          sampName =  samp
-                                   else: continue
-                                   for i in xrange(hist.GetNbinsX()):
-                                          for year in ["2016","2017","2018"]:
-                                                 sysName = "mcstat_%s_%s_MC%sbin%d      "  % (ch, sampName, year, i+1)
-                                                 card += "%-20s   shape   " % (sysName)
-                                                 card += line   
-                                                 card += "\n"    
-       
-              card += "\n"
-              
-       if mode == "template":
-              for par in parNames: card += "%-20s%-20s\n" % (par, "flatParam")
-
-       #card += "SF            extArg     1 [0.75,1.25]\n"
-       #card += "SF param 1 0.25\n"
-       #for par, formula in rateParams.iteritems(): 
-       #       formula = formula.replace("%s", '%.3f' % (eff))
-       #       card += "%-20s %-20s %-20s %-20s %s %-20s\n" % (par + "_rate", "rateParam", ch, sig, formula, "SF")
-
-
-      # card += "\n"
-
-       outname =  "%s_%s_%s.txt" % (sig, ch, mode)
-       cardfile = open(outname, 'w')
-       cardfile.write(card)
-       cardfile.close()
-
-       if bias:
-              card = card.replace(modelBkg.GetName(), "roomultipdf")
-              card.replace("rate                                    %-20.6f%-20.6f\n" % (1, 1), "rate                                    %-20.6f%-20.6f\n" % (10, 1))
-              card += "%-35s     discrete\n" % (pdf_index_string)
-              outname = "%s_%s_%s_bias.txt" % (sig, ch, mode)
-              cardfile = open(outname, 'w')
-              cardfile.write(card)
-              cardfile.close()
-    
-
-       #print card
-       return card
 
 
 
--- README.md
+++ README.md
@@ -27,3 +27,49 @@ To run all limits on the batch queques:
 > python getLimitData.py -y 2016 -d limitsRun2_v2/ -m template
 
 > python brazilPlot.py -y year -m template 
+
+
+# Updated method
+Be warned, the following is super cobbled together with bits of twig and some string
+
+Fisher testing and datacard writing are split into two jobs.
+First, to do the F-tests and generate the workspaces:
+
+> python createDatacardsFtest_pt1.py
+Options
+> -i inputFile directory, default is store/user/pedrok/SVJ2017/Datacards/trig4/sigfull
+> -d output directory. unused?
+> -m mode. default is hist, but template is more commonly used.
+> -t test, Bool flag. without -t, no bias testing is done
+> -u unblind Bool flag, also unused
+
+
+After part1 is done running, run part2:
+> python createDatacardsFtest_pt2.py
+options
+> -i inputFile directory, default is store/user/pedrok/SVJ2017/Datacards/trig4/sigfull
+> -d output directory. unused?
+> -m mode. default is hist, but template is more commonly used.
+> -Z, -D, -R, -A specifify which signal to use. defaults are 2900, 20, 03, peak
+> -t test, Bool flag. without -t, no bias testing is done
+> -u unblind Bool flag, also unused
+
+
+for Condor submission:
+make sure to change directories in the following files:
+Stat/Limits/test/condorScripts/scramTarEos.sh : lines 19, 27
+Stat/Limits/test/condorScripts/datacardCreationFtest_pt1.sh : lines 7, 37
+Stat/Limits/test/condorScripts/datacardCreationFtest_pt1.jdl : line 10 
+Stat/Limits/test/condorScripts/datacardCreationFtest_pt2.sh : lines 7, 48 (the pt2.jdl doesn't require any change)
+Stat/Limits/python/datacardsFtest_pt2.py : line 636
+
+step 1 is to create the workspace:
+> condor_submit datacardCreation_pt1.jdl
+
+step 2 is to write the datacards:
+> condor_submit datacardCreation_pt2.jdl
+
+step 3 is to run the combine commands to do the bias testing:
+> condor_submit combine_FourStepBias.jdl
+
+
