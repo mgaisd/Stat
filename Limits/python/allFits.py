afbe436220ee8d7d9a14b1a2669aa334bc16d450
Kevin Pedro <kpedro88@gmail.com>
2021-02-17 13:10:29 -0600

    run full brute force

--- Limits/python/allFits.py
+++ Limits/python/allFits.py
@@ -203,7 +203,8 @@ def getCard(sig, ch, ifilename, npool = 1, initvals = [1.0], bias = False, verbo
                             for pdfAlt in pdfsAlt:
                                 print "fit ",pdfAlt.name
                                 # truncate brute force at 3 param (4 takes too long, has some segfaults)
-                                mtmp, otmp, ftmp = bruteForce(pdfAlt, obsData, initvals, npool, [3,1.], 2)
+                                #mtmp, otmp, ftmp = bruteForce(pdfAlt, obsData, initvals, npool, [3,1.], 2)
+                                mtmp, otmp, ftmp = bruteForce(pdfAlt, obsData, initvals, npool, None, 2)
                                 modelAlt.append(mtmp)
                                 objsAlt.append(otmp)
                                 fitResAlt.append(ftmp)
