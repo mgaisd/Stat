7ce899058f1c2dc562d62d8cc87c7c244e29d6e2
Kevin Pedro <kpedro88@gmail.com>
2021-03-02 13:51:40 -0600

    add more options for impacts

--- Limits/test/impacts.sh
+++ Limits/test/impacts.sh
@@ -4,8 +4,19 @@ MASS=$1
 if [ -z "$MASS" ]; then
 	exit 1
 fi
-
 eosArea=$2
+TOYFILE=$3
+TOYNAME=$4
+
+toyArg=""
+if [ -n "$TOYFILE" ]; then
+	if [[ "$TOYFILE" == *"ASIMOV"* ]]; then
+		toyArg="-t -1"
+		TOYFILE=$(echo $TOYFILE | sed 's~ASIMOV~~')
+	else
+		toyArg="-t 1"
+	fi
+fi
 
 SVJ_NAME=SVJ_mZprime${MASS}_mDark20_rinv03_alphapeak
 if [ -n "$eosArea" ]; then
@@ -38,14 +49,19 @@ for COMBO in cut bdt; do
 	done
 	DC_NAME_ALL=datacard_${MASS}_${COMBO}
 	combineCards.py $DC_NAMES > ${DC_NAME_ALL}.txt
+	ARGS="--setParameters $SetArgAll --freezeParameters $FrzArgAll --trackParameters $TrkArgAll"
+	if [ -n "$TOYFILE" ]; then
+		ARGS="$ARGS --toysFile $(echo $TOYFILE | sed 's~COMBO~'${COMBO}'~') $toyArg --toysFrequentist"
+	fi
 
+	OUTNAME=impacts_${COMBO}${TOYNAME:+_}${TOYNAME}
 	text2workspace.py ${DC_NAME_ALL}.txt
-	combineTool.py -M Impacts -d ${DC_NAME_ALL}.root --doInitialFit --robustFit 1 -m 125 --setParameters $SetArgAll --freezeParameters $FrzArgAll --trackParameters $TrkArgAll
-	combineTool.py -M Impacts -d ${DC_NAME_ALL}.root --robustFit 1 -m 125 --doFits --parallel 8 --setParameters $SetArgAll --freezeParameters $FrzArgAll --trackParameters $TrkArgAll
-	combineTool.py -M Impacts -d ${DC_NAME_ALL}.root -o impacts_${COMBO}.json -m 125
-	plotImpacts.py -i impacts_${COMBO}.json  -o impacts_${COMBO}
+	combineTool.py -M Impacts -d ${DC_NAME_ALL}.root --doInitialFit --robustFit 1 -m 125 $ARGS
+	combineTool.py -M Impacts -d ${DC_NAME_ALL}.root --robustFit 1 -m 125 --doFits --parallel 8 $ARGS
+	combineTool.py -M Impacts -d ${DC_NAME_ALL}.root -o ${OUTNAME}.json -m 125
+	plotImpacts.py -i ${OUTNAME}.json  -o ${OUTNAME}
 	# subset
-	python excludeImpacts.py -i impacts_${COMBO}.json -m mcstat -x $(echo $FrzArgAll | tr ',' ' ')
-	plotImpacts.py -i impacts_${COMBO}_include.json  -o impacts_${COMBO}_include
-    plotImpacts.py -i impacts_${COMBO}_include.json -o impacts_${COMBO}_include_multi --per-page 4 --height 300 --label-size -1 --width 400
+	python excludeImpacts.py -i ${OUTNAME}.json -m mcstat -x $(echo $FrzArgAll | tr ',' ' ')
+	plotImpacts.py -i ${OUTNAME}_include.json  -o ${OUTNAME}_include
+    plotImpacts.py -i ${OUTNAME}_include.json -o ${OUTNAME}_include_multi --per-page 4 --height 300 --label-size -1 --width 400
 done
